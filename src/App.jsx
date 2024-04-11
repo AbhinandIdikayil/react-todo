be293fef7a238aebe35814da742a44ebebf47f9f
Abhinand <abhin4nd6@gmail.com>
2024-04-11 10:38:20 +0530

    changed key prop into uuid

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
-import { useState } from 'react'
+import { useRef, useState } from 'react'
 import Todo from './Todo'
 // import './App.css'
 
 
 function App() {
-
+  const value = useRef(0)
+  value.current = 10
+  console.log(value)
   return (
     <>
       <Todo />
--- src/Todo.jsx
+++ src/Todo.jsx
@@ -1,5 +1,7 @@
-import React, { useState } from 'react'
-
+import React, { useEffect, useRef, useState } from 'react'
+import { HiTrash } from "react-icons/hi2";
+import { FaEdit, FaLeaf } from "react-icons/fa";
+import {v4 as uuid} from 'uuid'
 import List from './components/List';
 import Swal from 'sweetalert2'
 function Todo() {
@@ -8,7 +10,8 @@ function Todo() {
   const [desc, setDesc] = useState('');
   const [tasks, setTasks] = useState([]);
   const [edit, setEdit] = useState(false);
-  const [index,setIndex] = useState(null) // this is to set the index of the task to know which index data is need to find
+  const [index, setIndex] = useState(null) // this is to set the index of the task to know which index data is need to find
+  const [showFinished,setShowFinished] = useState(false);
 
   function handleTitleInput(e) {
     setTitle(e.target.value)
@@ -20,13 +23,14 @@ function Todo() {
     let trimmedTitle = title.trim().length > 0
     let trimmedDesc = desc.trim().length > 0
     if (trimmedTitle && trimmedDesc) {
-      setTasks([...tasks, { title, desc }])
+      setTasks([...tasks, {id:uuid(), title, desc, isCompleted: false }])
       setTitle('')
       setDesc('')
     }
+    console.log(tasks)
   }
 
-  function confirm(ind, callback, tasks) {
+  function confirm(id) {
     Swal.fire({
       title: "Are you sure?",
       text: "You won't be able to revert this!",
@@ -42,14 +46,13 @@ function Todo() {
           text: "Your file has been deleted.",
           icon: "success"
         });
-        callback(ind, tasks)
+        deleteHandler(id)
       }
     });
   }
 
-  function deleteHandler(i, todoTask) {
-    let newTasks = todoTask.filter((data, index) => index !== i)
-    console.log(newTasks)
+  function deleteHandler(id) {
+    let newTasks = tasks.filter((data) => data.id !== id)
     setTasks(newTasks)
   }
   function editHandler(index) {
@@ -59,19 +62,28 @@ function Todo() {
     setIndex(index)
   }
   function saveEdited(index) {
-     let newTasks = tasks.map((data,ind) => {
-        if(ind == index){
-          data.title = title
-          data.desc = desc
-        }
-        return data
-     });
-    console.log(newTasks)
+    let trimmedTitle = title.trim().length > 0
+    let trimmedDesc = desc.trim().length > 0
+    let newTasks = tasks.map((data, ind) => {
+      if (ind == index && trimmedDesc && trimmedTitle) {
+        data.title = title
+        data.desc = desc
+      }
+      return data
+    });
     setTasks(newTasks);
     setTitle('')
     setDesc('')
     setEdit(false)
   }
+
+  function copmletedTaskHandler(index) {
+    let newTasks = [...tasks]
+    newTasks[index].isCompleted = !newTasks[index].isCompleted;
+    setTasks(newTasks)
+  }
+
+
   return (
     <>
       <div className='relative bg-zinc-800 w-full h-screen'>
@@ -81,47 +93,53 @@ function Todo() {
             className='placeholder:italic placeholder:text-black text-center bg-zinc-400 rounded-sm w-1/5 p-1 mt-7 text-white font-medium'
             value={title}
             onChange={handleTitleInput}
+            required={true}
             type="text" />
           <input
             placeholder='Enter description'
             className='placeholder:italic placeholder:text-black text-center bg-zinc-400 focus:outline-none rounded-sm w-2/5 p-2 text-white font-medium'
             value={desc}
             onChange={handleDescInput}
+            required={true}
             type="text" />
-
 
           <button
             onClick={() => edit ? saveEdited(index) : addTasks()}
             className='px-5 py-1 rounded-sm bg-zinc-950 font-semibold text-white'>
             {edit ? 'SAVE' : 'ADD'}
           </button>
-
-
+          
         </div>
 
         <h1 className='-z-9 absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 text-zinc-950 text-[6vw] text-center p-6 font-bold tracking-wider'>Todo list.</h1>
 
-        <div className="absolute w-full grid grid-cols-4 gap-4 px-20 z-1 mt-3">
+        <div className="absolute w-full grid grid-cols-4 gap-4 px-20 z-1 mt-5">
+
           {tasks.map((data, ind) => {
             return (
-              <List {...data} ind={ind} confirm={confirm} deleteHandler={deleteHandler} tasks={tasks} edit={editHandler} />
-              // <div key={ind} className='w-60 h-72 rounded-[20px] bg-zinc-900 overflow-hidden'>
-              //   <h2 className='px-5 py-3 text-xl font-semibold uppercase'>{data.title}</h2>
-              //   <div className='px-4 w-full h-48 overflow-auto'>
-              //     <p className='mt-2 font-semibold break-words'>
-              //       {data.desc}
-              //     </p>
-              //   </div>
-              //   <footer className='bg-sky-600/70 w-100 h-auto flex px-5 py-3.5 justify-between items-center'>
-              //     <button>
-              //       <FaEdit />
-              //     </button>
-              //     <button className='text-red-600' onClick={() => confirm(ind)}>
-              //       <HiTrash />
-              //     </button>
-              //   </footer>
-              // </div>
+              <div key={data.id} className='w-60 h-72 rounded-[20px] bg-zinc-900 overflow-hidden'>
+                <h2 className={`px-5 py-3 text-xl font-semibold uppercase  ${data.isCompleted ? "line-through" : ''}`}>{data.title}</h2>
+                <div className='px-4  w-full h-48 overflow-auto'>
+                  <p className={`mt-2 font-semibold break-words ${data.isCompleted ? "line-through" :''}`}>
+                    {data.desc}
+                  </p>
+                </div>
+                <footer className='bg-sky-600/70 w-100 h-auto flex px-5 py-3.5 justify-between items-center'>
+
+                  <input onChange={() => copmletedTaskHandler(ind)} type="checkbox" />
+                  <button onClick={() => editHandler(ind)}>
+                    <FaEdit />
+                  </button>
+                  <button className='text-red-600' onClick={() => confirm(data.id)}>
+                    <HiTrash />
+                  </button>
+
+                </footer>
+              </div>
             )
+
+            {/* <List {...data} ind={ind} confirm={confirm} deleteHandler={deleteHandler} tasks={tasks} edit={editHandler} /> */ }
+
           })}
         </div>
 
